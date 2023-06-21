684a288aec43cbb83c15c2315e1600ad4c209b46
adamt94 <thompson.ad94@gmail.com>
2023-06-21 12:51:46 +0100

    added Contactinfo Component

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,28 @@
+import { Component } from "solid-js";
+
+interface ContactInfo {
+  heading: string;
+  subheading: string;
+  image?: string;
+  children?: any;
+}
+
+const ContactInfo = (props: ContactInfo) => {
+  const { heading, subheading, image } = props;
+  return (
+    <nav class="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center surface-variant border-l-2 outline-variant-border">
+      <div class="flex items-center">
+        <div>
+          <img class="w-10 h-10 rounded-full" src={image} />
+        </div>
+        <div class="ml-4">
+          <p class="surface-tint-text title-medium">{heading}</p>
+          <p class="on-surface-variant-text title-small">{subheading}</p>
+        </div>
+      </div>
+      <div class="flex">{props.children}</div>
+    </nav>
+  );
+};
+
+export default ContactInfo;
