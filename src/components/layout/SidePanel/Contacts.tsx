b0a0a2cdaa233ac7f942fabf531b8774fbce441e
adamt94 <thompson.ad94@gmail.com>
2023-07-11 22:50:28 +0100

    added section headers for contacts

--- src/components/layout/SidePanel/Contacts.tsx
+++ src/components/layout/SidePanel/Contacts.tsx
@@ -12,54 +12,70 @@ export type ContactsProps = {
 export default function Contacts(props: ContactsProps) {
   const [expandedState, setExpandedState] = createSignal(true);
 
-  return (
-    <Motion.aside class="flex surface-tint-5 flex-col h-full overflow-hidden">
-      <div class=" h-full">
-        <Presence>
-          <Motion.div
-            initial={{ opacity: 0, x: -10 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0.1, duration: 0.5 }}
-            exit={{ opacity: 0, x: -10 }}
-          >
-            <nav class=" py-2 px-1 flex primary flex-row items-center h-16 mb-5">
-              <div class="flex items-center">
-                <IconButton label="back" onClick={props.onBackClick}>
-                  <IoArrowBackOutline class=" headline-small  on-primary-text" />
-                </IconButton>
-              </div>
-              <div class="flex items-center">
-                <p class="headline-small on-primary-text">Contacts</p>
-              </div>
-            </nav>
-            <div class="title-large on-primary-container-text mx-5">A</div>
-            <div class="h-px on-primary-container my-2 mx-3 w-auto"></div>
 
-            <For each={props.contacts} fallback={<div>No contacts yet.</div>}>
-              {(contact) => (
-                <div class="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
-                  <div class="flex items-center">
-                    <img
-                      class="w-10 h-10 rounded-full"
-                      src={contact.profilePicture}
-                      onError={(event) => {
-                        event.currentTarget.src =
-                          "https://i.stack.imgur.com/34AD2.jpg";
-                      }}
-                    />
-                    <div class="ml-4">
-                      <p class="surface-tint-text title-medium">
-                        {contact.name}
-                      </p>
-                      <p class="on-surface-variant-text title-small"></p>
+
+const getSectionHeaders = () => {
+  const firstLetters = new Set(
+    props.contacts.map((contact) => contact.name[0])
+  );
+  return Array.from(firstLetters).sort();
+};
+
+return (
+  <Motion.aside class="flex surface-tint-5 flex-col h-full overflow-hidden">
+    <div class=" h-full">
+      <Presence>
+        <Motion.div
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.1, duration: 0.5 }}
+          exit={{ opacity: 0, x: -10 }}
+        >
+          <nav class=" py-2 px-1 flex primary flex-row items-center h-16 mb-5">
+            <div class="flex items-center">
+              <IconButton label="back" onClick={props.onBackClick}>
+                <IoArrowBackOutline class=" headline-small  on-primary-text" />
+              </IconButton>
+            </div>
+            <div class="flex items-center">
+              <p class="title-large on-primary-text">Contacts</p>
+            </div>
+          </nav>
+          {getSectionHeaders().map((letter) => (
+            <>
+              <div class="title-large on-primary-container-text mx-5">
+                {letter}
+              </div>
+              <div class="flex items-center">
+                <div class="flex-1 h-px on-primary-container my-2 mx-3"></div>
+              </div>
+              {props.contacts
+                .filter((contact) => contact.name[0] === letter)
+                .map((contact) => (
+                  <div class="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center hover:surface-tint-1 hover:cursor-pointer">
+                    <div class="flex items-center">
+                      <img
+                        class="w-10 h-10 rounded-full"
+                        src={contact.profilePicture}
+                        onError={(event) => {
+                          event.currentTarget.src =
+                            "https://i.stack.imgur.com/34AD2.jpg";
+                        }}
+                      />
+                      <div class="ml-4">
+                        <p class="surface-tint-text title-medium">
+                          {contact.name}
+                        </p>
+                        <p class="on-surface-variant-text title-small"></p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
-            </For>
-          </Motion.div>
-        </Presence>
-      </div>
-    </Motion.aside>
-  );
+                ))}
+            </>
+          ))}
+        </Motion.div>
+      </Presence>
+    </div>
+  </Motion.aside>
+);
 }
